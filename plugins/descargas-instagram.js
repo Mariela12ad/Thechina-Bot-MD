4e64832004ee94535125a88b92faf14858a5829d
Mariela12ad <sosalopezmariela@gmail.com>
2024-11-22 03:46:47 -0600

    Update descargas-instagram.js

--- plugins/descargas-instagram.js
+++ plugins/descargas-instagram.js
@@ -9,7 +9,6 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   }
 
   if (!args[0]) throw `✳️ ${mssg.useCmd}\n *${usedPrefix + command}* (link unavailable)`
-  conn.sendReact(m.chat, '⏳️', m.key)
   try {
     let res = await fetch(global.API('fgmods', '/api/downloader/igdl', { url: args[0] }, 'apikey'))
     if (!res.ok) throw `❎ ${mssg.error} `
